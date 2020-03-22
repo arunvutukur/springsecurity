1fc63daa24cfc257834f4c02818ea0c0bdc4b388
arun <arunv04@gmail.com>
2020-03-23 01:17:10 +0530

    Added Spring Security filter 2

---
+++ src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,16 @@
+package main.java.com.luv2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	public SecurityWebApplicationInitializer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SecurityWebApplicationInitializer(Class<?>... configurationClasses) {
+		super(configurationClasses);
+		// TODO Auto-generated constructor stub
+	}
+
+}
