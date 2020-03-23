063ee2aa4d765a4cb129de33a4fb87110e85f04b
arun <arunv04@gmail.com>
2020-03-24 00:20:53 +0530

    Added logout option in homepage

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -37,7 +37,8 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 			.formLogin()
 				.loginPage("/showMyLoginPage")
 				.loginProcessingUrl("/authenticateTheUser")
-				.permitAll();
+				.permitAll()
+				.and().logout().permitAll();
 		
 	}
 	
